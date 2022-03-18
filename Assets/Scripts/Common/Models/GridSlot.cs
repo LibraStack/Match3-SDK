1ac30805497168b3b25c1145da51cdd71954df70
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2022-03-18 12:58:23 +0700

    Added asserts to GridSlot.

--- Assets/Scripts/Common/Models/GridSlot.cs
+++ Assets/Scripts/Common/Models/GridSlot.cs
@@ -21,11 +21,15 @@ namespace Common.Models
 
         public void Lock()
         {
+            Debug.Assert(Item == null, "Can not lock a grid slot while it has an item.");
+
             State = GridSlotState.NotAvailable;
         }
 
         public void Unlock()
         {
+            Debug.Assert(State == GridSlotState.NotAvailable, "Can not change {State} state to {GridSlotState.Free}.");
+
             State = GridSlotState.Free;
         }
 
@@ -37,11 +41,20 @@ namespace Common.Models
 
         public void MarkSolved()
         {
+            if (State == GridSlotState.Solved)
+            {
+                return;
+            }
+
+            Debug.Assert(State == GridSlotState.Occupied, "Can not mark an unoccupied grid slot as solved.");
+
             State = GridSlotState.Solved;
         }
 
         public void Clear()
         {
+            Debug.Assert(State != GridSlotState.NotAvailable, "Can not clear an unavailable grid slot.");
+
             Item = null;
             State = GridSlotState.Free;
         }
