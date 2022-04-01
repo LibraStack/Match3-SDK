d5badbe84bfbc3bce04849bccf2f4ff807580aa2
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2022-04-01 22:11:32 +0700

    Refactoring.

--- Assets/Scripts/Common/LevelGoalsProvider.cs
+++ Assets/Scripts/Common/LevelGoalsProvider.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Common.Interfaces;
 using Common.LevelGoals;
 using Match3.App;
@@ -8,16 +7,6 @@ namespace Common
 {
     public class LevelGoalsProvider : ILevelGoalsProvider<IUnityItem>
     {
-        // private Dictionary<int, LevelGoal<IUnityItem>[]> _levelGoals;
-        //
-        // public LevelGoalsProvider()
-        // {
-        //     _levelGoals = new Dictionary<int, LevelGoal<IUnityItem>[]>
-        //     {
-        //         { 0, new LevelGoal<IUnityItem>[] { new CollectRowMaxItems(gameBoard) } }
-        //     };
-        // }
-        
         public LevelGoal<IUnityItem>[] GetLevelGoals(int level, IGameBoard<IUnityItem> gameBoard)
         {
             return new LevelGoal<IUnityItem>[] { new CollectRowMaxItems(gameBoard) };
