a8989f8dc4ec5f9155caae68376a164d2c0ccb72
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2022-04-22 13:06:56 +0700

    Refactoring.

--- samples/Unity.Match3/Assets/Scripts/Common/GridTiles/States/IceState.cs
+++ samples/Unity.Match3/Assets/Scripts/Common/GridTiles/States/IceState.cs
@@ -5,22 +5,22 @@ namespace Common.GridTiles.States
     public class IceState : StatefulGridTile
     {
         private bool _isLocked = true;
-        private TileGroup _group = TileGroup.Ice;
+        private int _group = (int) TileGroup.Ice;
 
-        public override int GroupId => (int) _group;
+        public override int GroupId => _group;
         public override bool IsLocked => _isLocked;
         public override bool CanContainItem => true;
 
         protected override void OnComplete()
         {
             _isLocked = false;
-            _group = TileGroup.Available;
+            _group = (int) TileGroup.Available;
         }
 
         protected override void OnReset()
         {
             _isLocked = true;
-            _group = TileGroup.Ice;
+            _group = (int) TileGroup.Ice;
         }
     }
 }
--- samples/Unity.Match3/Assets/Scripts/Common/GridTiles/States/StoneState.cs
+++ samples/Unity.Match3/Assets/Scripts/Common/GridTiles/States/StoneState.cs
@@ -6,9 +6,9 @@ namespace Common.GridTiles.States
     {
         private bool _isLocked = true;
         private bool _canContainItem;
-        private TileGroup _group = TileGroup.Stone;
+        private int _group = (int) TileGroup.Stone;
 
-        public override int GroupId => (int) _group;
+        public override int GroupId => _group;
         public override bool IsLocked => _isLocked;
         public override bool CanContainItem => _canContainItem;
 
@@ -16,14 +16,14 @@ namespace Common.GridTiles.States
         {
             _isLocked = false;
             _canContainItem = true;
-            _group = TileGroup.Available;
+            _group = (int) TileGroup.Available;
         }
 
         protected override void OnReset()
         {
             _isLocked = true;
             _canContainItem = false;
-            _group = TileGroup.Stone;
+            _group = (int) TileGroup.Stone;
         }
     }
 }
